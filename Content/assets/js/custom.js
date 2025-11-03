abe54ab50cca560e74783bda7c86d9e849c1355d
awadhesh2410 <123048020+awadhesh2410@users.noreply.github.com>
2025-11-03 09:56:43 +0530

    Some UI Update
    Some UI Update

---
+++ Content/assets/js/custom.js
@@ -0,0 +1,58 @@
+ jQuery(document).ready(function(){
+	// LEFT MENU SCROLLBAR CODE
+	//$('.scroll_bar').enscroll({
+	//	verticalTrackClass: 'track4',
+	//	verticalHandleClass: 'handle4',
+	//	minScrollbarLength: 28
+	//});
+	//$('.datepicker').datepicker({
+	//	format: 'mm/dd/yyyy',
+	//	todayHighlight: true,
+	//	autoclose: true
+	//});
+	// $('.exampleDatatable').DataTable( {
+	//	paging: true,
+	//	"bFilter" : false,               
+	//	"bLengthChange": false,
+	//	searching: false,
+	//	orderCellsTop: true,
+ //       "scrollX": true,
+ //       "scrollCollapse": true,
+	//	responsive: true
+ //   });
+	//$('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+ //     $($.fn.dataTable.tables(true)).DataTable()
+ //     .columns.adjust();
+	//}); 
+	// LEFT MENU START CODE
+	//$(".menu-toggle").on("click", function(a) {
+	//	var b = $(this),
+	//		c = b.next();
+	//	if ($(b.parents("ul")[0]).hasClass("list")) {
+	//		var d = $(a.target).hasClass("menu-toggle") ? a.target : $(a.target).parents(".menu-toggle");
+	//		$.each($(".menu-toggle.toggled").not(d).next(), function(a, b) {
+	//			$(b).is(":visible") && ($(b).prev().toggleClass("toggled"), $(b).slideUp())
+	//		})
+	//	}
+	//	b.toggleClass("toggled"), c.slideToggle(320)
+	//});
+});
+
+//$(document).on("click", ".uploadBtn", function() {
+//	var file = $(this).parents().find(".fileBtn");
+//	file.trigger("click");
+//});
+//$('input[type="file"]').change(function(e) {
+//	var fileName = e.target.files[0].name;
+//	$("#fileBtn").val(fileName);
+//	var reader = new FileReader();
+//	reader.onload = function(e) {
+//		// get loaded data and render thumbnail.
+//		document.getElementById("preview").src = e.target.result;
+//	};
+//	// read the image file as a data URL.
+//	reader.readAsDataURL(this.files[0]);
+//});
+
+
+
